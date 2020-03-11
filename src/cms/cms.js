bc82c7d1f451024b8a8ce911ba3a14a384a81aba
Nick York <nick@limecreative.com>
2020-03-11 11:09:55 +0000

    Added cloudinary

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -5,6 +5,13 @@ import AboutPagePreview from './preview-templates/AboutPagePreview'
 import WorkPreview from './preview-templates/WorkPreview'
 import PricingPagePreview from './preview-templates/PricingPagePreview'
 import ContactPagePreview from './preview-templates/ContactPagePreview'
+// You only need to import the media library that you'll use. We register both
+// here for example purposes.
+import uploadcare from 'netlify-cms-media-library-uploadcare';
+import cloudinary from 'netlify-cms-media-library-cloudinary';
+
+CMS.registerMediaLibrary(uploadcare);
+CMS.registerMediaLibrary(cloudinary);
 
 CMS.init()
 CMS.registerPreviewStyle('/styles.css')
@@ -13,3 +20,4 @@ CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('pricing', PricingPagePreview)
 CMS.registerPreviewTemplate('contact', ContactPagePreview)
 CMS.registerPreviewTemplate('work', WorkPreview)
+
